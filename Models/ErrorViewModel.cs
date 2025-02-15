806a506ce74373e3a9db951e88eaae8c1172583b
Maksim Dziatkou <maxdet05@yandex.ru>
2025-02-15 23:29:04 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VersesWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Verse.cs
@@ -0,0 +1,13 @@
+﻿namespace VersesWebApp.Models
+{
+    public class Verse
+    {
+        public int Id { get; set; }
+        public string VerseText { get; set; }
+        public string Author { get; set; }
+        public Verse()
+        {
+            
+        }
+    }
+}
